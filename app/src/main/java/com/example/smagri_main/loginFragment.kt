4629ba43e7f26846e763262894af3fd2b41a38d5
mozarik <mozarikfahrozi@gmail.com>
2020-06-05 18:45:35 +0700

    Add navigation between Login and Register fragment, and Adding the Onclick Listener to all Button.

--- app/src/main/java/com/example/smagri_main/loginFragment.kt
+++ app/src/main/java/com/example/smagri_main/loginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
@@ -17,9 +18,21 @@ class login : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
-        view.loginText.setOnClickListener {
+
+        //After we hit the login button on the login fragment we navigate to Home Fragment
+        view.login_login_btn.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Login button on Login Fragment has Ben Clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        // After tap the register button on login layout, it goes to register layout
+        view.register_login_btn.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_login_to_registerFragment2)
         }
+
         return view
     }
 
--- app/src/main/java/com/example/smagri_main/registerFragment.kt
+++ app/src/main/java/com/example/smagri_main/registerFragment.kt
@@ -1,13 +1,19 @@
 package com.example.smagri_main
 
 import android.os.Bundle
+import android.text.method.Touch
+import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_login.view.*
+import kotlinx.android.synthetic.main.fragment_register.*
 import kotlinx.android.synthetic.main.fragment_register.view.*
+import kotlinx.android.synthetic.main.fragment_register.view.register_register_btn
 
 
 class registerFragment : Fragment() {
@@ -18,9 +24,16 @@ class registerFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_register, container, false)
-        view.RegisterText.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_login)
+
+        //Afer clicking Register button on Register fragment we need to register the user.
+        view.register_register_btn.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Register button on Register Fragment is clicked",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+
         return view
     }
 
